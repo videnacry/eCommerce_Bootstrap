a9d993c48fb34744c25d44ff1771fab5a4982c5f
EloyRDev <rodriguezeloy99@gmail.com>
2020-06-17 18:11:27 +0200

    New add product menu $ Add product event listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
 $(document).ready(() => {
+    
+    //EVENT LISTENERS
     $("a").click(e => {
         $(".manager-menu").hide()
         $("" + e.target.getAttribute("data-href") + "").show()
     })
+
+    $("#add_product_btn").click(addProduct)
 })
+
+function addProduct() {
+    const name = $("#apf_product_name")
+    const description = $("#apf_product_description")
+    const img = $("#apf_product_image")
+    const price = $("#apf_product_price")
+    const stock = $("#apf_product_stock")
+    const color = $("#apf_product_color")
+    //Checking if the form is correct and filled
+
+}
