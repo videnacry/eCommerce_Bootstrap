e74c4448ec960748b5058776cc40bc63a429b2cc
beron <bleink@outlook.es>
2020-06-18 19:18:27 +0200

    added ids and jquery

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,17 @@ $(document).ready(() => {
 
     $("#add_product_btn").click(addProduct)
 
+    //change style
+    $("#shipping-info").fadeOut()
+    showValidation($("#checkout-email"),"nice job!","is-valid","valid-feedback")
+    showValidation($("#up-to-date").parent(),"cool!","is-valid","valid-feedback")
+    showValidation($("#checkout-name"),"Hey, that's not valid","is-invalid","invalid-feedback")
+    showValidation($("#checkout-phone"),"Hey, your phone is required to contact you","is-invalid","invalid-feedback")
+    $("#customer-info form").submit(function(event){event.preventDefault()})
+    $("#continue-to-shipping").click(function(event){
+        alert("a")
+        replace($("#customer-info"),$("shipping-info"))
+    })
 })
 
 
@@ -23,3 +34,19 @@ function addProduct() {
     
 }
 
+function replace(element,replace){
+    element.fadeOut()
+    replace.fadeIn()
+}
+
+/**
+ * It creates feedback with the message and classFeedback and append it to the element, and add classElement to element
+ * @param {*jqueryElement} element 
+ * @param {*string} message 
+ * @param {*string} classElement 
+ * @param {*string} classFeedback 
+ */
+function showValidation(element,message,classElement,classFeedback){
+    let feedback = $("<div class="+classFeedback+">"+message+"</div>")
+    element.addClass(classElement).after(feedback)
+}
