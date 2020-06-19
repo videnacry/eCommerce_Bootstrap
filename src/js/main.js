24cc10398f1dc8043dc006d0fc0d80929db8f889
EloyRDev <rodriguezeloy99@gmail.com>
2020-06-19 10:10:20 +0200

    Comments and check for same name

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+/*S> DOCUMENT LOAD*/
+
 $(document).ready(() => {
     //EVENT LISTENERS
     $("a").click(e => {
@@ -12,7 +14,7 @@ $(document).ready(() => {
         drawCategories()
         $("#add_product").children("h2").text("Add Product")
         $("#add_product_btn").text("Add Product")
-        $("#add_product_btn").click(addProduct)
+        $("#add_product_btn").click(createProduct)
     })
 
     $("#btnCreateUser").click(e => {
@@ -28,25 +30,28 @@ $(document).ready(() => {
     drawProductList()
 })
 
+/*E> DOCUMENT LOAD*/
+/******************************************************************************************************************************************************/
+/*S> DATA WORK*/
+
 let data = getStorage() || {
     products: [],
     categories: [],
     users: []
 }
 
-function getStorage() { 
-    return JSON.parse(localStorage.getItem("data"))
-}
+function getStorage() { return JSON.parse(localStorage.getItem("data")) }
+function saveStorage() { localStorage.setItem("data", JSON.stringify(data)) }
 
-function saveStorage() {
-    localStorage.setItem("data", JSON.stringify(data))
-}
+/*E> DATA WORK*/
+/******************************************************************************************************************************************************/
+/*S> DRAW ELEMENTS FUNCTIONS*/
 
 function drawProductList() {
     $(".pl_product").remove()
     $("#products_list").show()
 
-    let i = 1
+    let i = 0
     for(const prod of data.products) {
         $("#pl_list").append(`
         <tr class="no-bs-dark-2 pl_product">
@@ -56,15 +61,62 @@ function drawProductList() {
             <td>${prod.stock}</td>
             <td><button type="button" class="btn btn-primary pl_edit_btn" data-productId="${i}">Edit</button></td>
         </tr>`)
+        i++
     }
 
     $(".pl_edit_btn").click(e => {
         const id = e.target.getAttribute("data-productId")
-        showUpdateProduct(data.products[id-1])
+        showUpdateProduct(data.products[id])
     })
 }
 
-function addProduct(product) {
+function drawCategories() {
+    $(".apf_product_category").parent().remove()
+    $(".apf_error").remove()
+
+    if(data.categories.length == 0) {
+        $("#apf_product_categories").append(`<div class="apf_error alert alert-danger mt-1 p-1">There are no categories created, 
+        create one before adding a product</div>`)
+    }
+
+    let id = 0
+    for(const cat of data.categories) {
+        $("#apf_product_categories").append(`
+        <div class="custom-control custom-checkbox p-1 ml-4">
+            <input type="checkbox" class="custom-control-input apf_product_category" name="${cat.name}" id="apf_product_cat_${id}">
+            <label class="custom-control-label" for="apf_product_cat_${id}">${cat.name}</label>
+        </div>`)
+        id++
+    }
+}
+
+function showUpdateProduct(product) {
+    $(".manager-menu").hide()
+    $("#add_product").show()
+    
+    $("#add_product").children("h2").text("Update Product")
+    $("#add_product_btn").text("Update Product")
+    drawCategories()
+
+    $("#apf_product_name").val(product.name)
+    $("#apf_product_description").val(product.description)
+    $("#apf_product_image").val(product.img)
+    $("#apf_product_price").val(product.price)
+    $("#apf_product_stock").val(product.stock)
+    $("#apf_product_weight").val(product.weight)
+    $("#apf_product_color").val(product.color)
+    
+    for(const cat of $(".apf_product_category"))
+        if(searchForSameName(product.categories, cat.name)) cat.checked = true
+        
+    $("#add_product_btn").click(() => createProduct(product))
+}
+
+/*E> DRAW ELEMENTS FUNCTIONS*/
+/******************************************************************************************************************************************************/
+/*S> CREATE OBJECT FUNCTIONS*/
+
+function createProduct(product) {
     const name = $("#apf_product_name")
     const description = $("#apf_product_description")
     const img = $("#apf_product_image")
@@ -78,10 +130,12 @@ function addProduct(product) {
     let validate = true
     $(".apf_error").remove()
 
-    if(name.val().length < 3) {
+    if(name.val().length < 3 || searchForSameName(data.products, name.val())) {
         validate = false
 
-        if(name.val().length == 0)
+        if(searchForSameName(data.products, name.val()))
+            name.after(`<div class="apf_error alert alert-danger mt-1 p-1">There is a product already with this name</div>`)
+        else if(name.val().length == 0)
             name.after(`<div class="apf_error alert alert-danger mt-1 p-1">Product name is required</div>`)
         else 
             name.after(`<div class="apf_error alert alert-danger mt-1 p-1">Product name has to be at least 3 characters</div>`)
@@ -163,7 +217,7 @@ function addProduct(product) {
         categories: selectedCategories
     }    
 
-    if(product !== undefined) data.products.splice(data.products.indexOf(product), 1)
+    if(product !== undefined) data.products.slice(data.products.indexOf(product), 1)
     data.products.push(newProduct)
     saveStorage()
 
@@ -176,31 +230,45 @@ function createUser()
 {
     $(".apf_error").remove()
 
-    let name = $("#inputUserName");
-    let email = $("#inputUserEmail");
-    let pass = $("#inputUserPass");
+    const name = $("#inputUserName");
+    const email = $("#inputUserEmail");
+    const pass = $("#inputUserPass");
 
     let validate = true
 
-    if(name.val() == "" || name.val().length < 3)
+    if(name.val() == "" || name.val().length < 3 || searchForSameName(data.users, name.val()))
     {
-        name.after('<div class="apf_error alert alert-danger mt-1 p-1">enter a valid name</div>')
+        if(searchForSameName(data.products, name.val()))
+            name.after('<div class="apf_error alert alert-danger mt-1 p-1">Username is taken</div>')
+        else if(name.val().length < 3)
+            name.after('<div class="apf_error alert alert-danger mt-1 p-1">Username has to be at least 3 characters long</div>')
+        else 
+            name.after('<div class="apf_error alert alert-danger mt-1 p-1">Username is required</div>')
+
         validate = false
     }
-    if(email.val() == "")
+    if(email.val() == "" || !email.val().includes("@"))
     {
-        email.after('<div class="apf_error alert alert-danger mt-1 p-1">enter a valid email example: example@mail.com</div>')
+        if(!email.val().includes("@"))
+            email.after('<div class="apf_error alert alert-danger mt-1 p-1">Enter a valid email. Example: example@mail.com</div>')
+        else
+            email.after('<div class="apf_error alert alert-danger mt-1 p-1">Email is required</div>')
+
         validate = false
     }
     if(pass.val() == "" || pass.val().length < 8)
     {
-        pass.after('<div class="apf_error alert alert-danger mt-1 p-1">enter a valid password</div>')
+        if(pass.val().length < 8)
+            pass.after('<div class="apf_error alert alert-danger mt-1 p-1">Enter a valid password</div>')
+        else 
+            pass.after('<div class="apf_error alert alert-danger mt-1 p-1">Password is required</div>')
+
         validate = false
     }
 
     if(!validate) return
 
-    let newUser = {
+    const newUser = {
         name: name.val(),
         email: email.val(),
         password: pass.val()
@@ -223,8 +291,12 @@ function createCategory()
 
     let validate = true
 
-    if (title.val().length == 0){
-        title.after('<div class="apf_error alert alert-danger mt-1 p-1">Category name is required</div>')
+    if (title.val().length == 0 || searchForSameName(data.categories, title.val())){
+
+        if(searchForSameName(data.categories, title.val()))
+            title.after('<div class="apf_error alert alert-danger mt-1 p-1">There is a category already with this name</div>')
+        else
+            title.after('<div class="apf_error alert alert-danger mt-1 p-1">Category name is required</div>')
         validate = false
     }
 
@@ -235,8 +307,8 @@ function createCategory()
 
     if(!validate) return
 
-    let newCategory = {
-        title: title.val(),
+    const newCategory = {
+        name: title.val(),
         color: color.val()
     }
 
@@ -248,58 +320,20 @@ function createCategory()
     drawProductList()
 }
 
-
-function drawCategories() {
-    $(".apf_product_category").parent().remove()
-    $(".apf_error").remove()
-
-    if(data.categories.length == 0) {
-        $("#apf_product_categories").append(`<div class="apf_error alert alert-danger mt-1 p-1">There are no categories created, 
-        create one before adding a product</div>`)
-    }
-
-    let id = 0
-    for(const cat of data.categories) {
-        $("#apf_product_categories").append(`
-        <div class="custom-control custom-checkbox p-1 ml-4">
-            <input type="checkbox" class="custom-control-input apf_product_category" name="${cat.title}" id="apf_product_cat_${id}">
-            <label class="custom-control-label" for="apf_product_cat_${id}">${cat.title}</label>
-        </div>`)
-        id++
-    }
-}
-
-function showUpdateProduct(product) {
-    $(".manager-menu").hide()
-    $("#add_product").show()
-    
-    $("#add_product").children("h2").text("Update Product")
-    $("#add_product_btn").text("Update Product")
-    drawCategories()
-
-    $("#apf_product_name").val(product.name)
-    $("#apf_product_description").val(product.description)
-    $("#apf_product_image").val(product.img)
-    $("#apf_product_price").val(product.price)
-    $("#apf_product_stock").val(product.stock)
-    $("#apf_product_weight").val(product.weight)
-    $("#apf_product_color").val(product.color)
-    
-    for(const cat of $(".apf_product_category"))
-        if(searchMatchCategory(product, cat.name)) cat.checked = true
-        
-    $("#add_product_btn").click(() => addProduct(product))
-}
+/*E> CREATE OBJECT FUNCTIONS*/
+/******************************************************************************************************************************************************/
+/*S> HELPER FUNCTIONS*/
 
 //Helper function to transform category name to object
 function categoryNameToObj(name) {
     for(const cat of data.categories) 
-        if(cat.title == name) return cat
+        if(cat.name == name) return cat
 }
 
-//Helper function to search for a category in a product
-function searchMatchCategory(product, name) {
-    for(const cat of product.categories)
-        if(cat.title == name) return true
+//Helper function to search for existent objects by name
+function searchForSameName(list, name) {
+    for(const elem of list)
+        if(elem.name == name) return true
 }
 
+/*E> HELPER FUNCTIONS*/
